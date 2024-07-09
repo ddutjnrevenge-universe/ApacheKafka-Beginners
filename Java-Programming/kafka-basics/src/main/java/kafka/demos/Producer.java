93cc1464b55b7e76c138db9ce6876efc230c6870
ddutjnrevenge <hanhnguyen210704@gmail.com>
2024-07-09 16:42:56 +0700

    adding producer demo and rebuild file structure

---
+++ Java-Programming/kafka-basics/src/main/java/kafka/demos/Producer.java
@@ -0,0 +1,22 @@
+package kafka.demos;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Producer {
+    private static final Logger log = LoggerFactory.getLogger(Producer.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("Hello World");
+//        System.out.println("Hello World");
+
+        //create Producer properties
+
+        // create Producer
+
+        // send data to Kafka
+
+        // flush and close producer
+    }
+}
+
+
--- Java-Programming/src/main/java/kafka/demos/Producer.java
+++
@@ -1,7 +0,0 @@
-package kafka.demos;
-
-public class Producer {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
