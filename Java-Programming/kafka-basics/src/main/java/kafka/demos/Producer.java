e52e4355e94e93c9b5c99bc6042da3a95333d2ad
ddutjnrevenge <hanhnguyen210704@gmail.com>
2024-07-10 16:26:32 +0700

    producer demo

--- Java-Programming/kafka-basics/src/main/java/kafka/demos/Producer.java
+++ Java-Programming/kafka-basics/src/main/java/kafka/demos/Producer.java
@@ -1,6 +1,10 @@
 package kafka.demos;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import java.util.Properties;
 
 public class Producer {
     private static final Logger log = LoggerFactory.getLogger(Producer.class.getSimpleName());
@@ -10,12 +14,25 @@ public class Producer {
 //        System.out.println("Hello World");
 
         //create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+
+        // set producer property
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
 
         // create Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a Producer record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo", "goodbye galaxy");
 
         // send data to Kafka
+        producer.send(producerRecord);
 
         // flush and close producer
+        producer.flush();
+        producer.close();
     }
 }
 
